5f149a8b942c6139b41b23f229ed238bdd2ea0ea
OwenChick06 <Ochickering25@gcds.net>
2024-09-10 13:53:23 -0400

    Add files via upload

---
+++ cjwff.py
@@ -0,0 +1,13 @@
+# Owen Chickering
+# Mail Sorter
+# Log
+# Bugs
+# Features
+# Sources
+
+data = input("Enter your shipping info")
+
+dimensions = data.split(",")
+h = dimension[0]
+l = dimensions[1]
+w = dimensions[2]
